b907121df9531750acbd0de6edc54e9edda87dee
Yahani Hewagama <yahanihewagama@yahoo.com>
2012-03-30 22:36:53 +0530

    second commit on src

--- src/studentmanagementsystem/SmsStarter.java
+++ src/studentmanagementsystem/SmsStarter.java
@@ -5,14 +5,26 @@
 
 package studentmanagementsystem;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 
 public class SmsStarter {
 
 	public static void main(String[] args) {
-		StudentManagementSystem sms;
-		//load the bean from spring
-//		sms.listAllStudents();
-//		sms.registerStudent(stu);
-//		sms.listAllStudents();
+		 XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource(
+"spring.xml"));
+        Student st1 = (Student)beanFactory.getBean("stdnt1");
+        Student st2 = (Student)beanFactory.getBean("stdnt2");
+        Student st3 = (Student)beanFactory.getBean("stdnt3");
+        Student st4 = (Student)beanFactory.getBean("stdnt4");
+
+        StudentManagementSystem sms = (StudentManagementSystem)beanFactory.getBean("spring");
+        sms.listAllStudents();
+        sms.registerStudent(st1);
+        sms.registerStudent(st2);
+        sms.registerStudent(st3);
+        sms.registerStudent(st4);
+        sms.listAllStudents();
+
 	}
 }
--- src/studentmanagementsystem/StudentManagementSystem.java
+++ src/studentmanagementsystem/StudentManagementSystem.java
@@ -5,12 +5,23 @@
 
 package studentmanagementsystem;
 
+import java.util.List;
+
 public class StudentManagementSystem {
 
 	private StudentRepository repo;
 
+        public StudentManagementSystem() {
+        this.repo = new SimpleStudentRepository();
+    }
 	public void listAllStudents() {
-            repo.findAllStudents();
+            List a;
+        a = repo.findAllStudents();
+        for (int i = 0; i < a.size(); i++) {
+            Student stu = (Student) a.get(i);
+            System.out.println(stu.getFirstName() + " " + stu.getLastName() + " " + stu.getAddress() + " " + stu.getRegNumber());
+        }
+
 	}
 
 	public void registerStudent(Student stu){
